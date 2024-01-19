2d57a3bad88d2b70f52d92a928d3269d18812a5c
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-01-19 11:19:47 +0000

    feat: allow creating new items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
-
 
 @app.route('/')
 def index():
     saved_items = get_items()
     return render_template('index.html', saved_items=saved_items)
+
+@app.route('/', methods=['POST'])
+def add():
+    title = request.form.get('title')
+    add_item(title)
+    return redirect(url_for('index'))
