fd4e7c7759ffe11050fac6497a00ae11727b93e3
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-02-07 15:49:13 +0000

    feat: use trello for updating status and name (and update on name box blur)

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_item, save_item, delete_item
-from todo_app.data.trello_items import get_items, add_item
+from todo_app.data.session_items import delete_item
+from todo_app.data.trello_items import get_items, add_item, update_name, update_status
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,7 +9,6 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     saved_items = get_items()
-    print(saved_items['incompleted_items'])
     return render_template('index.html', incompleted_items=saved_items['incompleted_items'], completed_items=saved_items['completed_items'])
 
 @app.route('/add', methods=['POST'])
@@ -22,10 +21,9 @@ def add():
 @app.route('/complete', methods=['POST'])
 def complete():
     item_id = request.form.get('item_id')
-    if item_id:
-        item = get_item(item_id)
-        item['completed'] = not item['completed']
-        save_item(item)
+    status = request.form.get('status')
+    if item_id and status:
+        update_status(item_id, status)
     return redirect(url_for('index'))
 
 @app.route('/edit', methods=['POST'])
@@ -33,9 +31,7 @@ def edit():
     title = request.form.get('title')
     item_id = request.form.get('item_id')
     if title and item_id:
-        item = get_item(item_id)
-        item['title'] = title
-        save_item(item)
+        update_name(item_id, title)
     return redirect(url_for('index'))
 
 @app.route('/delete', methods=['POST'])
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -17,4 +17,16 @@ def list_items(result_json, list_id):
     return [list['cards'] for list in result_json if list['id'] == list_id][0]
 
 def add_item(title):
-    requests.post(f'https://api.trello.com/1/cards?idList={trello_incompleted_list_id}&name={title}&{trello_board_id}&key={trello_api_key}&token={trello_api_token}')
+    requests.post(f'https://api.trello.com/1/cards?idList={trello_incompleted_list_id}&name={title}&key={trello_api_key}&token={trello_api_token}')
+
+def update_name(card_id, title):
+    requests.put(f'https://api.trello.com/1/cards/{card_id}?name={title}&key={trello_api_key}&token={trello_api_token}')
+
+def update_status(card_id, current_status):
+    match current_status:
+        case "completed":
+            list_id = trello_incompleted_list_id
+        case "incompleted":
+            list_id = trello_completed_list_id
+    print(list_id)
+    requests.put(f'https://api.trello.com/1/cards/{card_id}?idList={list_id}&key={trello_api_key}&token={trello_api_token}')
