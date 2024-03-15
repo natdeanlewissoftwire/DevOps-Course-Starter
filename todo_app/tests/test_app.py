801bd10b66504b9cdb2dc81e3887dd47ad6ea7a5
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-03-15 11:44:11 +0000

    test: test index page request

---
+++ todo_app/tests/test_app.py
@@ -0,0 +1,41 @@
+import pytest, requests, os
+from dotenv import load_dotenv, find_dotenv
+from todo_app import app
+
+@pytest.fixture
+def client():
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    test_app = app.create_app()
+
+    with test_app.test_client() as client:
+        yield client
+
+class StubResponse():
+    def __init__(self, fake_response_data):
+        self.fake_response_data = fake_response_data
+
+    def json(self):
+        return self.fake_response_data
+
+def stub(method, url, params={}):
+    test_board_id = os.environ.get('TRELLO_BOARD_ID')
+
+    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
+        fake_response_data = [{
+            'id': '123abc',
+            'name': 'To Do',
+            'cards': [{'id': '456', 'name': 'Test card'}]
+        }]
+        return StubResponse(fake_response_data)
+
+    raise Exception(f'Integration test did not expect URL "{url}"')
+
+def test_index_page(monkeypatch, client):
+    monkeypatch.setattr(requests, 'request', stub)
+
+    response = client.get('/')
+
+    assert response.status_code == 200
+    assert 'Test card' in response.data.decode()
