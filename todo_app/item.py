aad37f3e7201e590d2328e670d73816c86acbe2b
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-09-27 16:21:45 +0100

    feat: remove trello reference

--- todo_app/item.py
+++ todo_app/item.py
@@ -3,7 +3,3 @@ class Item:
         self.id = id
         self.name = name
         self.status = status
-
-    @classmethod
-    def from_trello_card(cls, card, list):
-        return cls(card['id'], card['name'], list['name'])
