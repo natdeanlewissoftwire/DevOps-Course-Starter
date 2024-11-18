1096b655c44aa78689b491f1b4c0f88c9292ea47
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-11-18 08:08:26 +0000

    feat: create terraform web app

--- main.tf
+++ main.tf
@@ -15,3 +15,25 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
   name     = "cohort32-33_NatDea_ProjectExercise"
 }
+
+resource "azurerm_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = "WicrosoftToDoTerraform"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image_name     = "appsvcsample/python-helloworld:latest"
+      docker_registry_url   = "https://index.docker.io"
+    }
+  }
+}
