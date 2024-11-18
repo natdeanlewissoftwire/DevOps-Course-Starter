32b1c91e2ccb9f2748f5cda3214192d043d55fa0
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-11-18 07:54:02 +0000

    feat: terraform init

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.8"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "d33b95c7-af3c-4247-9661-aa96d47fccc0"
+}
+
+data "azurerm_resource_group" "main" {
+  name     = "cohort32-33_NatDea_ProjectExercise"
+}
