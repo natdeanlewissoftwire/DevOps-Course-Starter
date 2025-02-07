277922a568c1bfe729da6f06ed52a9e0d804f544
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2025-02-07 15:31:44 +0000

    feat: add basic TODO manipulation logs

--- todo_app/app.py
+++ todo_app/app.py
@@ -6,8 +6,11 @@ from todo_app.flask_config import Config
 from todo_app.data.mongodb_items import get_items, add_item, update_name, update_status, delete_item
 from todo_app.user import User
 from todo_app.view_model import ViewModel
+import logging
+logger = logging.getLogger(__name__)
 
 def create_app(environ=None, start_response=None):
+    logging.basicConfig(filename='wicrosoft-to-do.log', level=logging.INFO)
     app = Flask(__name__)
     app.config.from_object(Config())
     login_manager = LoginManager()
@@ -59,6 +62,7 @@ def create_app(environ=None, start_response=None):
     def add():
         title = request.form.get('title')
         if title:   
+            app.logger.info("Adding item with title %s", title)
             add_item(title)
         return redirect(url_for('index'))
 
@@ -68,6 +72,7 @@ def create_app(environ=None, start_response=None):
         item_id = request.form.get('item_id')
         status = request.form.get('status')
         if item_id and status:
+            app.logger.info("Changing status of item %s", item_id)
             update_status(item_id, status)
         return redirect(url_for('index'))
 
@@ -77,6 +82,7 @@ def create_app(environ=None, start_response=None):
         title = request.form.get('title')
         item_id = request.form.get('item_id')
         if title and item_id:
+            app.logger.info("Editing item %s", item_id)
             update_name(item_id, title)
         return redirect(url_for('index'))
 
@@ -85,6 +91,7 @@ def create_app(environ=None, start_response=None):
     def delete():
         item_id = request.form.get('item_id')
         if item_id:
+            app.logger.info("Deleting item %s", item_id)
             delete_item(item_id)
         return redirect(url_for('index'))
     
